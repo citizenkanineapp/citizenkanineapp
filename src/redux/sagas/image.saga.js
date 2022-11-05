e2e96bdf41f0a15fd26f80d303e410e7b69f9f29
Sam Freeman <samcfreeman888@gmail.com>
2022-11-05 12:09:27 -0500

    restructured object for the reducer.  Need to rename data to URL for clarity

--- src/redux/sagas/image.saga.js
+++ src/redux/sagas/image.saga.js
@@ -11,30 +11,22 @@ function* uploadImage(action){
             url: '/api/image',
             data: action.payload
         })
-        yield put ({type: 'FETCH_IMAGE'});
+        console.log(image)
+        const {data} = image
+        const newImageObject = {data}
+        console.log('testing format', newImageObject)
+        //need to decide what type of state the reducer should be
+        yield put ({type: 'SET_DOG_PHOTO', payload: newImageObject});
     } catch {
         console.log('error in addImage')
     }
 }
 
-function* fetchImage (){
-    try {
-        const getImage = yield axios.get('/api/upload');
-        const imageArray = getImage.data;
-        const length = imageArray.length;
-        console.log(length)
-        console.log(getImage.data);
-        yield put ({type: 'SET_IMAGE', payload: getImage.data[length-1]});
-    } catch (error) {
-        console.log(error);
-        alert('Error fetching image');
-    }
-}
+// f
 
 function* imageSaga() {
     yield takeLatest('ADD_IMAGE', uploadImage);
-    yield takeLatest('FETCH_IMAGE', fetchImage)
-   
+    
   }
 
   export default imageSaga;

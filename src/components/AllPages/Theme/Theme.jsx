aad944bf8239b973bd1964ab54ef0e9bb776a44e
Sarah Preston <seprest@gmail.com>
2022-11-06 00:43:43 -0500

    added some css

--- src/components/AllPages/Theme/Theme.jsx
+++ src/components/AllPages/Theme/Theme.jsx
@@ -1,29 +1,88 @@
 import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
 
+const { palette } = createTheme();
+const font = "'Noto Sans', sans-serif";
+
+
 export const theme = createTheme({
-    palette: {
-        type: 'light',
-        primary: {
-            main: '#F8614D',
-            contrastText: 'rgba(255,255,255,0.87)',
-        },
-        secondary: {
-            main: '#539BD1',
-        },
-        error: {
-            main: '#f8614d',
-        },
-        warning: {
-            main: '#f5a572',
-        },
-        success: {
-            main: '#7BCEC8',
-        },
-        info: {
-            main: '#4a5061',
-        },
-        background: {
-            paper: '#fcf4eb',
-        },
+
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#e0603f',
+      contrastText: '#fbfaf8',
+      dark: '#fd8c2a',
     },
+    secondary: {
+      main: '#53b0d1',
+      contrastText: '#fbfaf8',
+      dark: '#53c8d1',
+    },
+    error: {
+      main: '#f8614d',
+      contrastText: '#fbfaf8',
+    },
+    warning: {
+      main: '#ff5e4a',
+      contrastText: '#fbfaf8',
+    },
+    success: {
+      main: '#7BCEC8',
+      contrastText: '#fbfaf8',
+    },
+    info: {
+      main: '#4a5061',
+    },
+    background: {
+      paper: '#fbfaf8',
+      default: '#ffffff',
+    },
+    navColor: palette.augmentColor({ color: {
+           main: "#fbfaf8"
+    } }),
+  },
+
+  typography: {
+    // fontFamily: font,
+    // button: { textTransform: "none" }
+    h4: {
+        fontWeight: "300",
+        fontSize: "2.5rem",
+      },
+  },
+  
+
+// palette: {
+//         type: 'light',
+//         primary: {
+//           main: '#ff5e4a',
+//           contrastText: '#fbfaf8',
+//           dark: '#fd8c2a',
+//         },
+//         secondary: {
+//           main: '#53b0d1',
+//           contrastText: '#fbfaf8',
+//           dark: '#53c8d1',
+//         },
+//         error: {
+//           main: '#f8614d',
+//           contrastText: '#fbfaf8',
+//         },
+//         warning: {
+//           main: '#FF7E1E',
+//           contrastText: '#fbfaf8',
+//         },
+//         success: {
+//           main: '#7BCEC8',
+//           contrastText: '#fbfaf8',
+//         },
+//         info: {
+//           main: '#4a5061',
+//         },
+//         background: {
+//           paper: '#f4f2ec',
+//           default: '#FFFFFF',
+//         },
+//       },
+      
 });

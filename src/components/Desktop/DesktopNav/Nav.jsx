be44f9a77ef9df2ec16744149087009a784a7898
Blake Smith <blake.smith.mn@gmail.com>
2022-11-05 20:02:22 -0500

    added functionality to buttons in the appbar drawer, in order to move around the app easier

--- src/components/Desktop/DesktopNav/Nav.jsx
+++ src/components/Desktop/DesktopNav/Nav.jsx
@@ -3,15 +3,23 @@ import { Link } from 'react-router-dom';
 import LogOutButton from '../../AllPages/LogOutButton/LogOutButton';
 import './Nav.css';
 import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
 
 // MUI IMPORTS
 import { Avatar, AppBar, Box, Divider, Drawer, IconButton, List, ListItem, ListItemButton, ListItemText, Toolbar, Typography, Button } from '@mui/material';
-
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import PetsIcon from '@mui/icons-material/Pets';
+import BadgeIcon from '@mui/icons-material/Badge';
+import EqualizerIcon from '@mui/icons-material/Equalizer';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const drawerWidth = 350;
 const navItems = ['Home', 'Pack Leaders', 'Clients', 'Reports'];
 
 function Nav(props) {
+
+  const history = useHistory();
   const user = useSelector((store) => store.user);
 
   const { window } = props;
@@ -26,13 +34,35 @@ function Nav(props) {
       <Avatar sx={{ width: 200, height: 200, fontSize: 50 }}>ADMIN</Avatar>
       <Divider />
       <List>
-        {navItems.map((item) => (
-          <ListItem disablePadding>
-            <ListItemButton sx={{ textAlign: 'center' }}>
-              <ListItemText primary={item} />
-            </ListItemButton>
-          </ListItem>
-        ))}
+
+        <ListItem >
+          <ListItemButton sx={{ textAlign: 'center' }} onClick={(event) => history.push('/user')}>
+            <DashboardIcon />
+            <ListItemText primary='Dashboard' />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem >
+          <ListItemButton sx={{ textAlign: 'center' }} onClick={(event) => history.push('/employees')}>
+            <BadgeIcon />
+            <ListItemText primary='Pack Leaders' />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem >
+          <ListItemButton sx={{ textAlign: 'center' }} onClick={(event) => history.push('/clients')}>
+            <PetsIcon />
+            <ListItemText primary='Clients' />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem>
+          <ListItemButton sx={{ textAlign: 'center' }} onClick={(event) => history.push('/invoice')}>
+            <EqualizerIcon />
+            <ListItemText primary='Reports' />
+          </ListItemButton>
+        </ListItem>
+
       </List>
       <Button color='error' variant='contained'>Log Out</Button>
     </Box>
@@ -44,15 +74,15 @@ function Nav(props) {
     <Box sx={{ display: 'flex' }}>
       <AppBar component="nav" >
         <Toolbar>
-          <Button onClick={handleDrawerToggle} color='secondary'>+</Button>
+          {/* <Button onClick={handleDrawerToggle} color='secondary'></Button> */}
           <IconButton
             color="inherit"
             aria-label="open drawer"
             edge="start"
             onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: 'none' } }}
+            sx={{ mr: 2, display: { sm: 'block' } }}
           >
-            {/* <MenuIcon /> */}
+            <MenuIcon />
           </IconButton>
           <Typography
             variant="h6"

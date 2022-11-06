18c155e82225d2afe72360b457bf60fe8f4df8f4
Blake Smith <blake.smith.mn@gmail.com>
2022-11-06 13:28:56 -0600

    built out form for schedule changes, added date picker from react calendar, and started logic for changing weekly schedule

--- src/components/Desktop/Client/ClientSchedule/ClientSchedule.jsx
+++ src/components/Desktop/Client/ClientSchedule/ClientSchedule.jsx
@@ -1,18 +1,110 @@
+import { useState } from 'react';
 import { useSelector, useDispatch } from "react-redux";
-
+//CALENDAR 
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 //MUI
-import Button from '@mui/material/Button';
+import { Card, CardContent, FormControl, InputLabel, MenuItem, Select, Paper, Avatar, AppBar, Box, Divider, IconButton, List, ListItem, ListItemButton, ListItemText, ListItemSecondaryAction, Typography, Button, Grid, TextField } from '@mui/material';
 
-function ClientSchedule(){
+function ClientSchedule() {
   const dispatch = useDispatch();
+  const [value, onChange] = useState(new Date());
+  const [dog, setDog] = useState('');
+  const [action, setAction] = useState('');
 
- return (
-  <div className="container">
-    <h1>Client Schedule</h1>
-    <Button onClick={() => dispatch({ type: 'SET_CLIENT_MODAL', payload: 'EditClientForm'})}>Back</Button>
-    <Button onClick={() => dispatch({ type: 'SET_CLIENT_MODAL', payload: 'ClientScheduleChanges'})}>Edit</Button>
-  </div>
- )    
+  const [walk, setWalk] = useState(false);
+
+  const handleClick = (event) => {
+    // toggle
+    setWalk(current => !current);
+    // console.log(event.target.value)
+  };
+
+  // NEEDS LOGIC FOR WEEKLY SCHEDULE STUFF BUT I DIDN'T WANT TO START A REDUCER AND ALL THAT
+
+  return (
+    <div className="container">
+      <h1>Client Name</h1>
+      <h2>Weekly Schedule</h2>
+      <Grid container spacing={2} sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }} >
+        <Grid item xs={2}>
+          <Card raised onClick={(event) => handleClick()} value={'Monday'}>
+            <CardContent sx={{ backgroundColor: walk ? '#7BCEC8' : null }}>
+              Monday
+            </CardContent>
+          </Card>
+
+        </Grid>
+        <Grid item xs={2} >
+          <Card raised sx={{ backgroundColor: '#7BCEC8' }}>
+            <CardContent>
+              Tuesday
+            </CardContent>
+          </Card>
+
+        </Grid>
+        <Grid item xs={2}>
+          <Card raised>
+            <CardContent>
+              Wednesday
+            </CardContent>
+          </Card>
+
+        </Grid>
+        <Grid item xs={2}>
+          <Card raised sx={{ backgroundColor: '#7BCEC8' }}>
+            <CardContent>
+              Thursday
+            </CardContent>
+          </Card>
+
+        </Grid>
+        <Grid item xs={2}>
+          <Card raised>
+            <CardContent>
+              Friday
+            </CardContent>
+          </Card>
+
+        </Grid>
+
+      </Grid>
+
+      <h2>Month View / Adjust Schedule</h2>
+
+      {/* OKAY so this actually becomes like a visualization of the calendar hopefully BUT we aren't there yet so  */}
+
+      <Grid container spacing={2} sx={{ mt: 2 }}>
+        <Grid item xs={6}>
+          <FormControl fullWidth sx={{ mr: 4, pb: 1 }}>
+            <InputLabel>Dog</InputLabel>
+            <Select value={dog} onChange={(event) => setDog(event.target.value)}>
+              <MenuItem value={'Spike'}>Spike</MenuItem>
+              <MenuItem value={'Fido'}>Fido</MenuItem>
+            </Select>
+          </FormControl>
+
+
+          <FormControl fullWidth sx={{ mr: 4, pb: 1 }}>
+            <InputLabel>Action</InputLabel>
+            <Select value={action} onChange={(event) => setAction(event.target.value)}>
+              <MenuItem value={'Add'}>Add Walk</MenuItem>
+              <MenuItem value={'Remove'}>Cancel Walk</MenuItem>
+            </Select>
+          </FormControl>
+          {/* THIS IS JUST A VIEW FOR THE DATE PICKER */}
+          <TextField value={value} fullWidth sx={{ mr: 4, pb: 1 }}></TextField>
+          <Button variant='contained' color='secondary'>Submit</Button>
+        </Grid>
+        <Grid item xs={6}>
+          <Calendar onChange={onChange} value={value} />
+        </Grid>
+
+      </Grid>
+      <Button onClick={() => dispatch({ type: 'SET_CLIENT_MODAL', payload: 'EditClientForm' })}>Back</Button>
+      <Button onClick={() => dispatch({ type: 'SET_CLIENT_MODAL', payload: 'ClientScheduleChanges' })}>Edit</Button>
+    </div >
+  )
 }
 
 export default ClientSchedule;

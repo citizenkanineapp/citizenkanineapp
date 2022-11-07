6e3d725d4726698b3cbe85289f183e30e016462f
Blake Smith <blake.smith.mn@gmail.com>
2022-11-07 15:21:00 -0600

    mobile home page set up to route to different parts of the mobile app ... like routes and schedule - also added log out button

--- src/components/Mobile/MobileNav/MobileNav.jsx
+++ src/components/Mobile/MobileNav/MobileNav.jsx
@@ -9,20 +9,22 @@ import PinDropIcon from '@mui/icons-material/PinDrop';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 
 function MobileNav() {
-
+  // state to show which link we are accessing at a time
   const [value, setValue] = useState(0);
+  // history to navigate us to different pages
   const history = useHistory();
 
   return (
+    // bottom navigation with conditional rendering that should only show on xs and small screens
     <BottomNavigation
       showLabels
       value={value}
       onChange={(event, newValue) => {
         setValue(newValue);
       }}
-      sx={{ display: { xs: 'flex', sm: 'block', md: 'none', position: 'fixed', bottom: 0, left: 0, right: 0, color: 'primary' } }}
+      sx={{ display: { xs: 'flex', sm: 'flex', md: 'none', position: 'fixed', bottom: 0, left: 0, right: 0, color: 'primary' } }}
     >
-      <BottomNavigationAction label="Home" icon={<HomeIcon />} onClick={(event) => { history.push('/user') }} />
+      <BottomNavigationAction label="Home" icon={<HomeIcon />} onClick={(event) => { history.push('/m/user') }} />
       <BottomNavigationAction label="Routes" icon={<ListAltIcon />} onClick={(event) => { history.push('/m/routes') }} />
       <BottomNavigationAction label="Map" icon={<PinDropIcon />} onClick={(event) => { history.push('/m/map') }} />
       <BottomNavigationAction label="Schedule" icon={<CalendarMonthIcon />} onClick={(event) => { history.push('/m/employees') }} />

1d5f4f22ea3dbe34de2b71fa9f85b0b92032afee
Blake Smith <blake.smith.mn@gmail.com>
2022-11-07 15:45:40 -0600

    added welcome message on mobile splash page

--- src/components/Mobile/Home/Home.jsx
+++ src/components/Mobile/Home/Home.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { Stack, CardContent, Card, Avatar, AppBar, Box, Divider, IconButton, List, ListItem, ListItemButton, ListItemText, ListItemSecondaryAction, Typography, Button, Grid, TextField } from '@mui/material';
+import { Paper, Stack, CardContent, Card, Avatar, AppBar, Box, Divider, IconButton, List, ListItem, ListItemButton, ListItemText, ListItemSecondaryAction, Typography, Button, Grid, TextField } from '@mui/material';
 import LogOutButton from '../../AllPages/LogOutButton/LogOutButton';
 
 function Home() {
@@ -10,10 +10,15 @@ function Home() {
 
   return (
     <Grid container spacing={2} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <Grid item xs={8} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', mt: 10 }}>
+      <Grid item xs={8} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', mt: 7 }}>
 
-        <Avatar sx={{ width: 200, height: 200, fontSize: 100, my: 5 }}>{user.username[0].toUpperCase() || ''}</Avatar>
+        <Avatar sx={{ width: 200, height: 200, fontSize: 100, my: 1 }}>{user.username[0].toUpperCase() || ''}</Avatar>
 
+      </Grid>
+      <Grid item xs={6}>
+        <Paper sx={{ p: 2, borderRadius: 5, textAlign: 'center', backgroundColor: '#539BD1', color: 'white' }}>
+          WELCOME {user.username.toUpperCase()}:
+        </Paper>
       </Grid>
       <Grid item xs={8}>
         <Stack direction="column" divider={<Divider orientation="vertical" flexItem />} spacing={1}>
--- src/components/Mobile/WalkerSchedule/WalkerSchedule.jsx
+++ src/components/Mobile/WalkerSchedule/WalkerSchedule.jsx
@@ -1,9 +1,31 @@
-function EmployeeSchedule(){
-    return (
-        <div className="container">
-          <h1>EmployeeSchedule</h1>
-        </div>
-      );
+import { useState } from 'react';
+import { useSelector, useDispatch } from "react-redux";
+
+//CALENDAR 
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+//MUI
+import { Chip, Card, CardContent, FormControl, InputLabel, MenuItem, Select, Paper, Avatar, AppBar, Box, Divider, IconButton, List, ListItem, ListItemButton, ListItemText, ListItemSecondaryAction, Typography, Button, Grid, TextField } from '@mui/material';
+
+function EmployeeSchedule() {
+
+  const [value, onChange] = useState(new Date());
+
+
+  return (
+    <Grid container sx={{ justifyContent: 'center', mt: 5 }}>
+      <Grid item xs={10}>
+        <h1>Employee Shedule</h1>
+      </Grid>
+
+      <Grid item sx={10}>
+
+        <Calendar onChange={onChange} value={value} />
+      </Grid>
+    </Grid>
+
+
+  );
 }
 
 export default EmployeeSchedule;

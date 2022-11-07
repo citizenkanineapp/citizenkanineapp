6c67d8a334c4c9d46708dabeea37933bca8017d7
Sam Freeman <samcfreeman888@gmail.com>
2022-11-07 13:47:13 -0600

    Believe get route for client list is ready and formatted to send to client.  Want to make notes on the code for clarity

--- server/routes/clients.router.js
+++ server/routes/clients.router.js
@@ -2,10 +2,15 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
+const {
+    rejectUnauthenticated,
+  } = require('../modules/authentication-middleware');
+
+
 /**
  * GET route template
  */
-router.get('/', (req, res) => {
+router.get('/', rejectUnauthenticated, (req, res) => {
   console.log('arrived in server get all route')
   const queryText = `
             SELECT clients.first_name, clients.id, clients.last_name, clients.address, dogs.name as dog_name from clients
@@ -16,6 +21,15 @@ router.get('/', (req, res) => {
   `
 pool.query(queryText)
     .then(result => {
+        //all IDs from database
+        let idArray = [];
+            for(let object of result.rows){
+                // console.log(object.id)
+                idArray.push(object.id)
+            }
+      
+      //this filters out duplicate IDs
+      let uniqueIds = [...new Set(idArray)]
 
     //this groups result.rows by id
       const group = result.rows.reduce((acc, item) => {
@@ -26,15 +40,20 @@ pool.query(queryText)
         acc[item.id].push(item);
         return acc;
       }, {})
-        console.log(result.rows);
+        // console.log(result.rows);
+        // console.log(group)
         let clients = [];
 
         //taking a break here.  This currently gives me a client object but it isn't filtering out duplicate IDs
         // Need to get dogs into an dog object and attach it to the client ID
         
-        for (let i = 0; i<result.rows.length; i++){
-            const {first_name, last_name, address} = result.rows[0];
+        for (let i = 0; i<uniqueIds.length; i++){
+            let forDogMap = group[uniqueIds[i]]
+       
+            // const {first_name, last_name, address} = result.rows[0];
+               const {first_name, last_name, address} = forDogMap[0];
             const client = {first_name, last_name, address}
+            client.dogs = forDogMap.map(dog => {return({dog_name: dog.dog_name})})
             clients.push(client)
 
         }

05bb7f7277ad23f3a922fd62c487871af3d4dc56
Blake Smith <blake.smith.mn@gmail.com>
2022-11-08 12:58:12 -0600

    address is clickable in the dog details view - at least while hard coded

--- src/components/Mobile/DogDetails/DogDetails.jsx
+++ src/components/Mobile/DogDetails/DogDetails.jsx
@@ -1,23 +1,44 @@
 import { useEffect, useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useHistory, Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { Paper, Stack, CardContent, Card, Avatar, AppBar, Box, Divider, IconButton, List, ListItem, ListItemButton, ListItemText, ListItemSecondaryAction, Typography, Button, Grid, TextField } from '@mui/material';
+import { Fab, CardMedia, Card, Paper, Stack, CardContent, Avatar, AppBar, Box, Divider, IconButton, List, ListItem, ListItemButton, ListItemText, ListItemSecondaryAction, Typography, Button, Grid, TextField } from '@mui/material';
+import EditIcon from '@mui/icons-material/Edit';
 
 
 function DogDetails() {
+  const schoolAddress = { address: '400 S 4th St', city: 'Minneapolis', zipCode: '55415', }
+  const clientAddress = { address: '4145 Zarthan AVE S', city: 'St. Louis Park', zipCode: '55416', }
+  const history = useHistory();
+
+  const openMap = async (clientAddress) => {
+    const destination = encodeURIComponent(`${clientAddress.address} ${clientAddress.zipCode}, ${clientAddress.city}`);
+    const link = `http://maps.google.com/?daddr=${destination}`;
+    window.open(link);
+  }
+
   return (
     <Grid container spacing={2}>
-      <Grid item>
+      <Grid item xs={12}>
         <Button>BACK</Button>
       </Grid>
       <Grid item xs={6}>
-        <Paper>DOG PHOTO</Paper>
+        <Card>
+          <Fab color="primary" aria-label="add" size='small' sx={{ position: 'fixed', mt: 1, ml: 1 }}>
+            <EditIcon />
+          </Fab>
+          <CardMedia component="img" height='70%' image="https://www.rd.com/wp-content/uploads/2020/11/GettyImages-889552354-e1606774439626.jpg" />
+        </Card>
       </Grid>
       <Grid item xs={6}>
         <Stack direction='column'>
-          <Card>DOG NAME</Card>
-          <Card>DOG ADDRESS</Card>
-          <Card><a href="tel:7637447704">CALL</a></Card>
+          <Card>
+            <Typography variant='h3'>RUFUS</Typography>
+          </Card>
+          <Card onClick={(event) => openMap(clientAddress)}>DOG ADDRESS</Card>
+          <Card onClick={(event) => openMap(schoolAddress)}>VET ADDRESS</Card>
+
+
+          <Card><a href="tel:+17637447704">CALL</a></Card>
         </Stack>
       </Grid>
       <Grid item xs={12}>

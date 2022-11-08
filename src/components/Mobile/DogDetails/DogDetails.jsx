d70ec017ae92b9212175f7ecf26325e454f3e343
Blake Smith <blake.smith.mn@gmail.com>
2022-11-08 16:08:09 -0600

    fleshed out dog details page example

--- src/components/Mobile/DogDetails/DogDetails.jsx
+++ src/components/Mobile/DogDetails/DogDetails.jsx
@@ -1,12 +1,15 @@
 import { useEffect, useState } from 'react';
 import { useHistory, Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { Fab, CardMedia, Card, Paper, Stack, CardContent, Avatar, AppBar, Box, Divider, IconButton, List, ListItem, ListItemButton, ListItemText, ListItemSecondaryAction, Typography, Button, Grid, TextField } from '@mui/material';
+import { Chip, Fab, CardMedia, Card, Paper, Stack, CardContent, Avatar, AppBar, Box, Divider, IconButton, List, ListItem, ListItemButton, ListItemText, ListItemSecondaryAction, Typography, Button, Grid, TextField } from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
+import FlagIcon from '@mui/icons-material/Flag';
 
 
 function DogDetails() {
-  const schoolAddress = { address: '400 S 4th St', city: 'Minneapolis', zipCode: '55415', }
+
+  const dog = { flagged: true };
+  const vetAddress = { address: '400 S 4th St', city: 'Minneapolis', zipCode: '55415', }
   const clientAddress = { address: '4145 Zarthan AVE S', city: 'St. Louis Park', zipCode: '55416', }
   const history = useHistory();
 
@@ -17,11 +20,13 @@ function DogDetails() {
   }
 
   return (
-    <Grid container spacing={2}>
+    <Grid container spacing={2} sx={{ justifyContent: 'center' }}>
+      {/* NAV BACK TO LIST */}
       <Grid item xs={12}>
         <Button>BACK</Button>
       </Grid>
-      <Grid item xs={6}>
+      {/* DOG PHOTO */}
+      <Grid item xs={5}>
         <Card>
           <Fab color="primary" aria-label="add" size='small' sx={{ position: 'fixed', mt: 1, ml: 1 }}>
             <EditIcon />
@@ -29,33 +34,58 @@ function DogDetails() {
           <CardMedia component="img" height='70%' image="https://www.rd.com/wp-content/uploads/2020/11/GettyImages-889552354-e1606774439626.jpg" />
         </Card>
       </Grid>
-      <Grid item xs={6}>
+      {/* DOG NAME */}
+      <Grid item xs={5}>
         <Stack direction='column'>
-          <Card>
-            <Typography variant='h3'>RUFUS</Typography>
+          <Card sx={{ mb: 1 }}>
+            <Typography variant='h4' align='center'>RUFUS</Typography>
           </Card>
-          <Card onClick={(event) => openMap(clientAddress)}>DOG ADDRESS</Card>
-          <Card onClick={(event) => openMap(schoolAddress)}>VET ADDRESS</Card>
-
-
-          <Card><a href="tel:+17637447704">CALL</a></Card>
+          {/* CLIENT ADDRESS INFORMATION */}
+          <Card onClick={(event) => openMap(clientAddress)}>
+            <Typography align='center' variant='caption'>
+              {clientAddress.address} {clientAddress.city} {clientAddress.zipCode}
+            </Typography>
+          </Card>
         </Stack>
       </Grid>
-      <Grid item xs={12}>
-        <Card>ACCESS DETAILS</Card>
+      {/* ACCESS INFORMATION */}
+      <Grid item xs={10}>
+        <Card>
+          <Typography align='center'>Side door - Access Code: 12543</Typography>
+        </Card>
       </Grid>
-      <Grid item xs={12}>
-        OWNER DETAILS
+      {/* OWNER INFORMATION & CALL BUTTON */}
+      <Grid item xs={10}>
+        <Card>
+          <CardContent>
+            <Typography variant='h6'>Owner Info:</Typography>
+            <Typography variant='body2'> Dolly Parton | <a href="tel:+17637447704">(763)-744-7704</a></Typography>
+          </CardContent>
+        </Card>
       </Grid>
-      <Grid item xs={12}>
-        VET DETAILS
+      {/* VET INFORMATION & CALL BUTTON */}
+      <Grid item xs={10}>
+        {/* <Card onClick={(event) => openMap(vetAddress)}> */}
+        <Card>
+          <CardContent>
+            <Typography variant='h6'>Vet Info:</Typography>
+            <Typography variant='body2'>Dr. Terry | <a href="tel:+16127159132">(612)-715-9132</a> </Typography>
+            <Typography variant='caption'>{vetAddress.address} {vetAddress.city} {vetAddress.zipCode}</Typography>
+          </CardContent>
+        </Card>
       </Grid>
-      <Grid>
-        DOG NOTES / WARNING ICON
+      {/* DOG NOTES & CONDITIONAL FLAG */}
+      <Grid item xs={10}>
+        <Card>
+          <Stack direction='row' alignItems='center'>
+            {dog.flagged ? <FlagIcon sx={{ fill: '#e0603f' }} /> : null}
+            <Typography variant='inherit'>
+              Rufus need time to warm up to new friends
+            </Typography>
+          </Stack>
+        </Card>
       </Grid>
     </Grid>
-
-
   );
 }
 

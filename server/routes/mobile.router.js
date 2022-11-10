22b2984101eabf42882d124d267f24b6a99bea30
Blake Smith <blake.smith.mn@gmail.com>
2022-11-09 19:49:43 -0600

    tested loop in PUT route - and seems to be working ... get route still needs logic for taking in a day object?

--- server/routes/mobile.router.js
+++ server/routes/mobile.router.js
@@ -1,4 +1,3 @@
-module.exports = router;
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
@@ -8,7 +7,7 @@ const {
 
 
 // GET ROUTE FOR DAILY DOGS SCHEDULE
-router.get('/', rejectUnauthenticated, (req, res) => {
+router.get('/', (req, res) => {
     console.log(req.body)
     // not sure exactly what our request body is going to look like
     // BUT this is generally what the request should look like? 
@@ -25,6 +24,15 @@ router.get('/', rejectUnauthenticated, (req, res) => {
 	WHERE daily_dogs.date = CURRENT_DATE
 	ORDER BY daily_dogs.route_id;
     `
+
+    pool.query(sqlQuery)
+        .then(response => {
+            res.send(response.rows);
+        })
+        .catch(error => {
+            console.log('ERROR IN GETTING DAILY DOGS:', error);
+            res.sensStatus(500);
+        })
     // GET route code here
 });
 
@@ -37,7 +45,9 @@ router.post('/', rejectUnauthenticated, (req, res) => {
 
 // PUT ROUTE TO UPDATE DOG ROUTES FOR THE DAY
 // if it's a put per move it's this:
-router.put('/:id', rejectUnauthenticated, (req, res) => {
+router.put('/', async (req, res) => {
+    const client = await pool.connect();
+
     // what is the req.body?
     console.log(req.body);
 
@@ -45,12 +55,35 @@ router.put('/:id', rejectUnauthenticated, (req, res) => {
     // in the route_id and the dog_id ... i figured the dog_id is more easily accessible than the actual table row key
 
     // Need to look into a DB connection promises thing
-    const sqlQuery = `
-    UPDATE daily_dogs
-	SET 
-		"route_id" = 3
-	WHERE "dog_id" = 3;
-    `
+    // const sqlQuery = `
+    // UPDATE daily_dogs
+    // SET 
+    // 	"route_id" = 3
+    // WHERE "dog_id" = 3;
+    // `
+
+    try {
+        // expect req.body to be an array of dogs 
+        // inner objects should be {dog_id: #, route_id: #}
+        const dogs = req.body.dogs;
+
+        await client.query('BEGIN')
+
+        await Promise.all(dogs.map(dog => {
+            const updateQuery = `UPDATE daily_dogs SET "route_id" = $1 WHERE "dog_id" = $2`;
+            const updateValues = [dog.route_id, dog.dog_id];
+            return client.query(updateQuery, updateValues);
+        }));
+
+        await client.query('COMMIT')
+        res.sendStatus(201);
+    } catch (error) {
+        await client.query('ROLLBACK')
+        console.log('Error POST /api/order', error);
+        res.sendStatus(500);
+    } finally {
+        client.release()
+    }
 });
 
 module.exports = router;

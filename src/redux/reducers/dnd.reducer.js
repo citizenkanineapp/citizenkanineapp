f682f6533dc052ed3fc2b242c0cbbdf4d3f123f9
Sarah Preston <seprest@gmail.com>
2022-11-09 20:46:13 -0600

    got dnd for multiple lists working

--- src/redux/reducers/dnd.reducer.js
+++ src/redux/reducers/dnd.reducer.js
@@ -15,35 +15,30 @@ const routes = (state = {}, action) => {
             const newRouteArray = state[action.payload.destination.droppableId];
             const oldIndex = action.payload.source.index;
             const newIndex = action.payload.destination.index;
-
-            console.log(oldIndex, newIndex);
-
-            const dogToMove = oldRouteArray.find(dog => dog.id === dogID); //dog that moves
-            
-            console.log(oldRouteArray, newRouteArray, dogToMove);
-        
-            // const oldRouteMinusDog = newState[oldRoute].filter(dog => dog.id !== dogID); //old route without moveddog
-            // console.log(newRoute);
-            // console.log(dogToMove);
+    
             //movement in the same route
-
             if (oldRouteName === newRouteName){
 
-                const reorderedRoute = Array.from(oldRouteArray);
-                const [dogToReorder] = reorderedRoute.splice(oldIndex, 1);
-                reorderedRoute.splice(newIndex, 0, dogToReorder);
+                const reorderedRoute = Array.from(oldRouteArray); //creates shallow copy of array
+                const [dogToReorder] = reorderedRoute.splice(oldIndex, 1); //removes dog from original index
+                reorderedRoute.splice(newIndex, 0, dogToReorder); //adds dog to new index
 
                 return {...state, [oldRouteName]: reorderedRoute};
             }
-            //movement from one route to another
-            // if (oldRoute !== newRoute){
-            // const dogWithNewRoute = {...dogToMove, route: newRoute} //changes dog's route property to the new route
-            // const newRouteWithAddedDog = newState[newRoute].splice(newIndex, 0, dogWithNewRoute);
 
-            // console.log(newState[newRoute]);
-            // const newnewState = {...newState, [oldRoute]: oldRouteMinusDog, [newRoute]: newRouteWithAddedDog};
-            // return newState;
-            //}
+
+            //movement from one route to another
+            if (oldRouteName !== newRouteName){
+
+                const sourceRoute = Array.from(oldRouteArray); //creates shallow copy of array
+                const [dogToMove] = sourceRoute.splice(oldIndex, 1); //removes dog from original array
+
+                const destinationRoute = Array.from(newRouteArray); //creates shallow copy of array
+                const ammendedDog = {...dogToMove, route: newRouteName} //changes dog's route property to the new route location
+                destinationRoute.splice(newIndex, 0, ammendedDog); //adds dog to new array
+
+                return {...state, [oldRouteName]: sourceRoute, [newRouteName]: destinationRoute};
+            }
         
         default:
             return state;

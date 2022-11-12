71d7281b1c6c7e442e60b5d48be1af643d862924
Yanira Sanchez <yani@Yaniras-MacBook-Pro.local>
2022-11-12 10:25:30 -0600

    added individual employee cards with emp avatars

--- src/components/Desktop/Employee/EmployeeSchedule/EmployeeSchedule.jsx
+++ src/components/Desktop/Employee/EmployeeSchedule/EmployeeSchedule.jsx
@@ -2,6 +2,10 @@ import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import dayjs from 'dayjs';
+// This plugin is needed to get the week number in year:
+var weekOfYear = require('dayjs/plugin/weekOfYear')
+dayjs.extend(weekOfYear)
+import './employeeSchedule.css';
 
 // Calender Imports:
 import format from "date-fns/format";
@@ -21,8 +25,7 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { CalendarPickerSkeleton } from '@mui/x-date-pickers/CalendarPickerSkeleton';
-import { Typography, Grid, Avatar, Card, CardContent, CardActionArea, Box } from "@mui/material";
-import './employeeSchedule.css';
+import { Typography, Grid, Avatar, CardActionArea, Box, Card, CardContent } from "@mui/material";
 
 
 
@@ -36,6 +39,9 @@ const isWeekend = (date) => {
 
   return day === 0 || day === 6;
 };
+
+const weeksInYear = Array.from({length: 53}, (_, i) => i + 1)
+console.log('array of weeks',weeksInYear.length)
 
 function EmployeeSchedule(){
   const dispatch = useDispatch();
@@ -54,7 +60,10 @@ function EmployeeSchedule(){
       type: 'SAGA_FETCH_EMPLOYEES'
     })
   },[]);
+  // console.log('first week of year',dayjs('2023-01-01').week())
+  // console.log('last week of year',dayjs('2022-12-30').week())
 
+  const allEmployees = useSelector(store=> store.employeesReducer.employees);
 
   const openModal = (view) => {
     dispatch({ type: 'SET_EMPLOYEE_MODAL', payload: view }); //assures the view to be the right component
@@ -69,168 +78,112 @@ function EmployeeSchedule(){
   // Calling dayjs() without parameters returns a fresh day.js object with the current date and time that looks like:
   // {$L: 'en', $u: undefined, $d: Sat Nov 05 2022 14:37:11 GMT-0500 (Central Daylight Time), $x: {…}, $y: 2022}
   // console.log(value);
-  // This 👇 state is for the basic datePicker for making changes. 
-  const [value2, setValue2] = useState(dayjs());
-  // 👇 will have to be replaced with the reducer for the clients schedule and the days will have to be in an array. 
-  const [highlightedDays, setHighlightedDays] = useState([1,2,4])
-  // console.log('value of StaticDatePicker',value.$W); // This returns an integer (1-5) representing the days of the week (M-F)
-  // console.log('value2:', value2.$W);
 
-  // Getting the week number in year:
-  var weekOfYear = require('dayjs/plugin/weekOfYear')
-  dayjs.extend(weekOfYear)
-
-  const thisWeek = dayjs(dayjs().$d).week();
-  console.log('this is week number:', thisWeek);
-  
-
-  // Employee schedule Logic:
   const oddEmpSchedules = useSelector(store=> store.employeesReducer.oddEmpSchedules);
   const evenEmpSchedules = useSelector(store=> store.employeesReducer.evenEmpSchedules);
-  // console.log(empSchedules);
-  // {1: true, 2: true, 3: false, 4: true, 5: true, id: 1, first_name: 'Den', last_name: 'Paolini', email: 'dpaolini0@paypal.com', phone: '(840)6732127', …}
-  const allEmployees = useSelector(store=> store.employeesReducer.employees);
+  // console.log(evenEmpSchedules);
+  // {1: true, 2: true, 3: false, 4: true, 5: true, id: 1, week: 2, first_name: 'Den', last_name: 'Paolini', email: 'dpaolini0@paypal.com', phone: '(840)6732127', …}
 
   const avatarColors = ['#4A5061', '#539BD1', '#7BCEC8', '#F9CB78', '#F5A572', '#F37E2D', '#F8614D', '#4A5061', '#539BD1', '#7BCEC8', '#F9CB78', '#F5A572', '#F37E2D', '#F8614D' ];
-  console.log(avatarColors[0]);
-
-  console.log(dayjs());
-
-
+  // console.log(dayjs());
   return (
-  <div className="container">
+  <Box sx={{display: 'flex', width: '100vw', justifyContent: 'center'}}>
     {/* // MUI DatePicker: */}
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <StaticDatePicker
-                disableHighlightToday={false}
-                orientation="portrait"
-                openTo="day"
-                value={value}
-                shouldDisableDate={isWeekend}
-                onChange={(newValue) => {
-                setValue(newValue);
-                }}
-                renderInput={(params) => <TextField key={day.$D} {...params} sx={{height: '80vh'}} />}
-                // render day loops through the days in the month and performs the given function. 
-                renderDay={(day, _value, DayComponentProps) => {
-                    // console.log('day is:', day.$W);
-                    // day.$W returns returns an integer (1-5) representing the days of the week (M-F)
-                    // const isSelected =
-                    //     !DayComponentProps.outsideCurrentMonth &&
-                    //     highlightedDays.includes(day.$W);
-
-                    const isSelected =
-                        !DayComponentProps.outsideCurrentMonth &&
-                        highlightedDays.includes(day.$W);
-                    
-                    {/* Need to check if the week is even or odd */}
-                    const weekInYear = dayjs(day.$d).week();
-                    // is this week even?
-
-                    return (
-                      <div key={day.$D}>
-                        <Box className="container"  sx={{display: 'flex', flexDirection: 'column', alignContent: 'flex-start', width: '8vw', height: '13vh', justifyContent: 'flex-start', border: 1}}>
-                          <Box sx={{display: 'flex', justifyContent: 'right', flexGrow: '1'}}>
-                            <PickersDay {...DayComponentProps} sx={{display: 'flex', alignContent: 'flex-start'}}/>
-                          </Box>
-                          {/* CONDITIONAL RENDERING FOR EMPLOYEE EVEN/ODD WEEKS */}
-                          {/* is the day within the current month and is this week even ? */}
-                          { !DayComponentProps.outsideCurrentMonth && weekInYear % 2 !== 0 ?
-                            <Box sx={{display:'flex', flexDirection: 'row', flexGrow: '7', alignContent: 'flex-start'}}>
-                            {oddEmpSchedules.map((employee, index) => {
-                              if (employee[day.$W]){
-                                const bgColor = avatarColors[index];
-                                return <Avatar key={employee.emp_id} sx={{ bgcolor: bgColor, height: '5vh' , width: '2.5vw' }}>{employee.first_name[0]}{employee.last_name[0]}</Avatar>
-                              }
+    <Box className="container" sx={{display: 'flex', flexDirection: 'row', mt: 1}}>
+      <Card variant="outlined" sx={{bgcolor: '#FCF4EB'}}>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <StaticDatePicker
+                    disableHighlightToday={false}
+                    orientation="portrait"
+                    openTo="day"
+                    value={value}
+                    shouldDisableDate={isWeekend}
+                    onChange={(newValue) => {
+                    setValue(newValue);
+                    }}
+                    renderInput={(params) => {
+                    // console.log(dayjs());
+                    <TextField key={day.$D} {...params} sx={{height: '80vh'}} />
+                    }}
+                    // render day loops through the days in the month and performs the given function. 
+                    renderDay={(day, _value, DayComponentProps) => {
+                        console.log(DayComponentProps );
+                        const currentYear = DayComponentProps.day.$y;
+                        // dayjs calculates weeks in year as a decimal that rounds up so the calculation for weekInYear accounts for this issue. Without this, the last week of the year would be week 53 and the first week of the year would be 1 which are both odd and would render an incorrect schedule. 
+                        const weekInYear = day.diff(`${currentYear}-01-01`, 'week', false)
+                        return (
+                            <Box key={day.$d} className="container"  sx={{display: 'flex', flexDirection: 'column', alignContent: 'flex-start', width: '8vw', height: '13vh', justifyContent: 'flex-start'}}>
+                              <Box sx={{display: 'flex', justifyContent: 'center', flexGrow: '1'}}>
+                                <PickersDay {...DayComponentProps} sx={{display: 'flex', alignContent: 'flex-start'}}/>
+                              </Box>
                               
-                            })}
+                              {/* CONDITIONAL RENDERING FOR EMPLOYEE EVEN/ODD WEEKS */}
+                              {/* is the day within the current month and is this week even(2)? */}
+                              {!DayComponentProps.outsideCurrentMonth?
+                                <>
+                                  {weekInYear % 2 !== 0  ?
+                                    <Box sx={{display:'flex', flexDirection: 'row', flexGrow: '7', justifyContent: 'center', alignContent: 'flex-start', flexWrap: 'wrap'}}>
+                                    {oddEmpSchedules.map((employee, index) => {
+                                      if (employee[day.$W]){
+                                        const bgColor = avatarColors[index];
+                                        return <Avatar key={employee.emp_id} sx={{ bgcolor: bgColor, height: '3vh' , width: '1.5vw', fontSize: 10, mx: .25 }}>{employee.first_name[0]}{employee.last_name[0]}</Avatar>
+                                      }
+                                      
+                                    })}
+                                    </Box>
+                                    :
+                                    <Box sx={{display:'flex', flexDirection: 'row', flexGrow: '7', justifyContent: 'center', alignContent: 'flex-start', flexWrap: 'wrap'}}>
+                                    {evenEmpSchedules.map((employee, index) => {
+                                    if (employee[day.$W]){
+                                      const bgColor = avatarColors[index];
+                                      return <Avatar key={employee.emp_id} sx={{ bgcolor: bgColor, height: '3vh' , width: '1.5vw', fontSize: 10, mx: .25 }}>{employee.first_name[0]}{employee.last_name[0]}</Avatar>
+                                    }
+                                    console.log('end of render')
+                                  })}
+                                  </Box>
+                                  }
+                                  </>
+                                : null 
+                              }
                             </Box>
-                            :
-                            <Box sx={{display:'flex', flexDirection: 'row', flexGrow: '7', alignContent: 'flex-start'}}>
-                            {evenEmpSchedules.map((employee, index) => {
-                            if (employee[day.$W]){
-                              const bgColor = avatarColors[index];
-                              return <Avatar key={employee.emp_id} sx={{ bgcolor: bgColor, height: '5vh' , width: '2.5vw' }}>{employee.first_name[0]}{employee.last_name[0]}</Avatar>
-                            }
-                            
-                          })}
-                          </Box>
-                          }
-                          {/* <Box sx={{display:'flex', flexDirection: 'row', flexGrow: '7', alignContent: 'flex-start'}}>
-                          {oddEmpSchedules.map((employee, index) => {
-                            if (employee[day.$W]){
-                              const bgColor = avatarColors[index];
-                              return <Avatar key={employee.emp_id} sx={{ bgcolor: bgColor, height: '5vh' , width: '2.5vw' }}>{employee.first_name[0]}{employee.last_name[0]}</Avatar>
-                          }
-                            
-                          })}
-                          </Box> */}
+                        );
+                    }}
+                />
+          </LocalizationProvider>
+        </Card>
 
-                          {/* { isSelected ? 
-                            <Box sx={{display: 'flex', justifyContent: 'center', flexGrow: '6'}}>
-                            this is where I would map the employees for the days
-                            <Avatar sx={{ bgcolor: "secondary.main", height: 20 , width: 20 }}>YH</Avatar>
-                          </Box>
-                          :
-                          null
-                          } */}
-                          
-                        </Box>
-                      </div>
-                      
-                        // <Badge
-                        //     key={day.$W}
-                        //     // overlap="circular"
-                        //     badgeContent={isSelected? '🐶' : undefined}
-                        // >
-                        //     <PickersDay {...DayComponentProps} />
-                        // </Badge>
-                    );
+        {/* <div className="display_calendar">
+          <p onClick={() => openModal('EmployeeDetails')}>Dolly Parton - Click me</p> 
+          <Button onClick={() => history.push('/employees')}>Back</Button>
+          <Button onClick={() => setShowEditCalendar(!showEditCalendar)}>Edit</Button> 
+        </div>
 
-                // {/* Testing StaticDatePicker without badges and just highlighting days of the week, not using a badge */}
-                // renderDay={(day, _value, DayComponentProps) => {
-                //   console.log('day is:', day.$W);
-                //   // day.$W returns returns an integer (1-5) representing the days of the week (M-F)
-                //   let selectedMUIClass='';
-                //   if (highlightedDays.includes(day.$W)){
-                //       selectedMUIClass ="MuiButtonBase-root MuiPickersDay-root Mui-selected MuiPickersDay-dayWithMargin css-bkrceb-MuiButtonBase-root-MuiPickersDay-root";
-                //   }
+        : //toggles between edit mode and viewing mode
 
-                //   return (
-                //           <PickersDay 
-                //           className={ selectedMUIClass }
-                //           {...DayComponentProps} />
-                //   );
+        <div className="edit_calendar">
+          <h2>Edit Calendar</h2>
+          <p onClick={() => openModal('EmployeeDetails')}>Dolly Parton WANTS TO WORK</p> 
+          <Button onClick={() => setShowEditCalendar(!showEditCalendar)}>Cancel</Button>
+          <Button onClick={() => setShowEditCalendar(!showEditCalendar)}>Submit</Button> 
+        </div> */}
 
-
-                }}
-            />
-        </LocalizationProvider>
-
-
-    {/* REACT CALENDAR STUFF */}
-    {/* <Calendar localizer={localizer}  startAccessor="start" endAccessor="end" style={{height: 500, margin: "50px"}} />
-    {showEditCalendar === false ?
-
-      <div className="display_calendar">
-        <h2>Display Calendar</h2>
-        <p onClick={() => openModal('EmployeeDetails')}>Dolly Parton - Click me</p> 
-        <Button onClick={() => history.push('/employees')}>Back</Button>
-        <Button onClick={() => setShowEditCalendar(!showEditCalendar)}>Edit</Button> 
-      </div>
-
-      : //toggles between edit mode and viewing mode
-
-      <div className="edit_calendar">
-        <h2>Edit Calendar</h2>
-        <p onClick={() => openModal('EmployeeDetails')}>Dolly Parton WANTS TO WORK</p> 
-        <Button onClick={() => setShowEditCalendar(!showEditCalendar)}>Cancel</Button>
-        <Button onClick={() => setShowEditCalendar(!showEditCalendar)}>Submit</Button> 
-      </div>} */}
-
-    <EmployeeModal/> 
-    {/* only open when button is pressed */}
-  </div>
+      <EmployeeModal/> 
+      {/* only open when button is pressed */}
+    </Box>
+    {/* employee cards */}
+    <Box sx={{display: 'flex', flexDirection: 'column', ml: 2, width: '30vw'}}>
+        {evenEmpSchedules.map( (employee, index)=> (
+          <Card sx={{display: 'flex', flexDirection: 'row', mt: 1}}>
+            <CardContent sx={{display: 'flex', flexDirection: 'row'}}>
+              <Avatar key={employee.emp_id} sx={{ bgcolor: avatarColors[index], height: '10vh' , width: '5vw', fontSize: 10, mr: 1, alignSelf: 'center' }}>{employee.first_name[0]}{employee.last_name[0]}</Avatar>
+              <Typography sx={{display: 'flex', alignSelf: 'center'}}>
+                {employee.first_name} {employee.last_name}
+              </Typography>
+            </CardContent>
+          </Card>
+        ))}
+    </Box>
+    </Box>
   )    
 }
 

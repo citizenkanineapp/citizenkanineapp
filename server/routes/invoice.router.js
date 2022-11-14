5fb8590d57a5765e7e9fc7a2167c4dc1c695a9ce
Dan Raskin <Danieltraskin@gmail.com>
2022-11-14 16:28:00 -0600

    sql query returns dates in array

--- server/routes/invoice.router.js
+++ server/routes/invoice.router.js
@@ -36,70 +36,61 @@ router.get('/', async (req, res) => {
     console.log(searchQuery);
 
     // query returns data object of each walk instance by customer-date
-    // AND walks-per week
     // 
     // query needs adjustment so that walks/per week is derived from client_schedule data, not actual walks per week.
 
+
+    // DOES DAILYDOGS CURRENTLY TRACK NO_SHOWS??????
+
     const queryWalkDetails = `
     SELECT
-        clients.id AS clientid,
-        daily_dogs.date,
-        daily_dogs.week_of_year,
-        COUNT(dogs.id) AS num_dogs,
-        results2.walks_per_week,
-        daily_dogs.checked_in AS checked_in,
-        daily_dogs.no_show AS no_show,
-        clients.last_name AS last_name,
-        clients.first_name AS first_name
-    FROM daily_dogs
-        JOIN "dogs"
-            ON daily_dogs.dog_id = dogs.id
-        JOIN clients
-            ON dogs.client_id = clients.id
-        INNER JOIN (
-            SELECT
-                id AS client_id,
-                week_of_year,
-                COUNT(week_of_year) AS walks_per_week
-            FROM (
-                SELECT
-                    daily_dogs.date,
-                    daily_dogs.week_of_year,
-                    COUNT(dogs.id) AS num_dogs,
-                    clients.id
-                FROM daily_dogs
-                    JOIN "dogs"
-                        ON daily_dogs.dog_id = dogs.id
-                    JOIN clients
-                        ON dogs.client_id = clients.id
-                GROUP BY
-                    daily_dogs.date,
-                    clients.id,
-                    daily_dogs.week_of_year
-            ) results
-                GROUP BY
-                    client_id,
-                    results.week_of_year
-        ) results2
-            ON (
-                results2.week_of_year = daily_dogs.week_of_year
-                AND dogs.client_id = results2.client_id
-            )`
-    + searchQuery +
-    `
-    GROUP BY
-        daily_dogs.date,
-        results2.walks_per_week,
-        checked_in, no_show,
-        last_name,
+        clientid,
         first_name,
+        last_name,
+        num_dogs,
+        ARRAY_AGG (
+            EXTRACT (MONTH FROM date)||'/' || EXTRACT (DAY FROM date)
+            ORDER by date ASC
+        ) dates,
+        checked_in,
+        no_show
+    FROM (	
+        SELECT
+            clients.id AS clientid,
+            daily_dogs.date,
+            daily_dogs.week_of_year,
+            COUNT(dogs.id) AS num_dogs,
+            daily_dogs.checked_in AS checked_in,
+            daily_dogs.no_show AS no_show,
+            clients.last_name AS last_name,
+            clients.first_name AS first_name
+        FROM daily_dogs
+            JOIN "dogs"
+                ON daily_dogs.dog_id = dogs.id
+            JOIN clients
+                ON dogs.client_id = clients.id`
+        + searchQuery +
+        `
+            GROUP BY
+                daily_dogs.date,
+                checked_in,
+                no_show,
+                last_name,
+                first_name,
+                clientid,
+                daily_dogs.week_of_year
+            ORDER BY
+                clientid,
+                date
+        ) results
+    GROUP BY
         clientid,
-        daily_dogs.week_of_year
-    ORDER BY
-        clientid,
-        date;
-`;
-
+        first_name,
+        last_name, 
+        num_dogs,
+        checked_in,
+        no_show;
+    `;
 
     try {
         const resDetails = await pool.query(queryWalkDetails,searchTerms);

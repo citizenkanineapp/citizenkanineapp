46078fefac12bcf43bdaed0c8a9861b2ea558670
Blake Smith <blake.smith.mn@gmail.com>
2022-11-17 16:11:41 -0600

    pool updates

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -10,21 +10,19 @@ const url = require('url');
 
 let config = {};
 
-if (process.env.DATABASE_URL) {
+if (process.env.DB_PASS) {
   // Heroku gives a url, not a connection object
   // https://github.com/brianc/node-pg-pool
-  const params = url.parse(process.env.DATABASE_URL);
-  const auth = params.auth.split(':');
+  // const params = url.parse(process.env.DATABASE_URL);
+  // const auth = params.auth.split(':');
 
   config = {
-    user: auth[0],
-    password: auth[1],
-    host: params.hostname,
-    port: params.port,
-    database: params.pathname.split('/')[1],
-    ssl: { rejectUnauthorized: false },
-    max: 10, // max number of clients in the pool
-    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+    user: 'citizenkanine',
+    host: 'db.bit.io',
+    database: 'citizenkanine/citizen_kanine',
+    password: process.env.DB_PASS, // key from bit.io database page connect menu
+    port: 5432,
+    ssl: true,
   };
 } else {
   config = {

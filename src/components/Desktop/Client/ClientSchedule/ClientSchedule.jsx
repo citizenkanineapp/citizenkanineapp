43586bdea84f94a51e93ab2ef988871a3ef33946
Yanira Sanchez <yani@Yaniras-MacBook-Pro.local>
2022-11-16 21:35:10 -0600

    cleaned up component by removing code not being used

--- src/components/Desktop/Client/ClientSchedule/ClientSchedule.jsx
+++ src/components/Desktop/Client/ClientSchedule/ClientSchedule.jsx
@@ -25,24 +25,19 @@ function ClientSchedule() {
   
 
       
-    // const clientSchedule= useSelector(store=> store.clientScheduleReducer);
-    const clientSchedule = {1: true, 2: true, 3: true, 4: true, 5: false}
-    // const dogs = useSelector(store=> store.clientReducer.dogs)
-    const dogs = [{dog_name: 'Cord', image: 'https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/domestic-dog_thumb_4x3.jpg', dog_id: 1, dog_notes: null, flag: null, regular: true}, {dog_name: 'Pamela', image: 'https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/domestic-dog_thumb_4x3.jpg', dog_id: 7, dog_notes: null, flag: null, regular: false}, {dog_name: 'Tami', image: 'https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/domestic-dog_thumb_4x3.jp', dog_id: 16, dog_notes: null, flag: null, regular: true}]
 
-    
-    const avatarColors = ['#4A5061', '#F5A572', '#7BCEC8', '#F9CB78', '#F5A572', '#F37E2D', '#F8614D', '#4A5061', '#539BD1', '#7BCEC8', '#F9CB78', '#F5A572', '#F37E2D', '#F8614D' ];
-    
-    // CALENDAR STUFF
-    // Testing to add a dog:
-    const changes =[{dog_id: 1, date_to_change: '2022-11-18', is_scheduled: false}, {dog_id: 7, date_to_change: '2022-11-22', is_scheduled: true}]
+  // CALENDAR STUFF
+  const clientSchedule = {1: true, 2: true, 3: true, 4: true, 5: false}
 
-    // console.log(dayjs('2022-11-22').$W); // returns 2 for Tuesday
-    // console.log(dayjs('2022-11-22').$d); // returns Tue Nov 22 2022 00:00:00 GMT-0600
-  const boxStyling = {display: 'flex', flexDirection: 'row', height: '4vw', width: '4.5vw', alignItems: 'flex-start', justifyContent:'center', mb: 0}
-  const avatarStyling = {width: '1vw', height: '1vw', mx: .25}
+  const dogs = [{dog_name: 'Cord', image: 'https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/domestic-dog_thumb_4x3.jpg', dog_id: 1, dog_notes: null, flag: null, regular: true}, {dog_name: 'Pamela', image: 'https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/domestic-dog_thumb_4x3.jpg', dog_id: 7, dog_notes: null, flag: null, regular: false}, {dog_name: 'Tami', image: 'https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/domestic-dog_thumb_4x3.jp', dog_id: 16, dog_notes: null, flag: null, regular: true}]
 
-  // {display: 'flex', flexDirection: 'row', flexGrow: '8', flexWrap: 'wrap',width: '5vw', alignContent: 'flex-start', justifyContent:'center', mb: 0}
+  const changes =[{id: 1, dog_id: 1, date_to_change: '2022-11-18', is_scheduled: false}, {id: 2, dog_id: 7, date_to_change: '2022-11-22', is_scheduled: true}]
+
+  const noChange = ()=> {// This is a blank function used as a placeholder for the onChange of the calendar picker. onChange is required but has no use in this case. 
+  }
+
+  const avatarColors = ['#4A5061', '#F5A572', '#7BCEC8', '#F9CB78', '#F5A572', '#F37E2D', '#F8614D', '#4A5061', '#539BD1', '#7BCEC8', '#F9CB78', '#F5A572', '#F37E2D', '#F8614D' ];
+
 
   return (
     <>
@@ -52,30 +47,17 @@ function ClientSchedule() {
             <CalendarPicker 
               className='clientSchedule'
               shouldDisableDate={isWeekend}
+              onChange={noChange}
               renderInput={(params) => {
                     // console.log(dayjs());
                     <TextField key={day.$D} {...params} />
                     }}
               // renderDay is essentially mapping through each day in the selected month.
               renderDay={(day, _value, DayComponentProps) => {
-                console.log(JSON.stringify(DayComponentProps.day.$d));
-                console.log(JSON.stringify(dayjs('2022-11-22').$d))
-                console.log(JSON.stringify(DayComponentProps.day.$d) === JSON.stringify(dayjs('2022-11-22').$d));
-                // console.log('day is:', day.$W);
-                // day.$W returns returns an integer (1-5) representing the days of the week (M-F)
                 let selectedMUIClass='';
-                // console.log(day)
                 if (day.$d === dayjs()){
                     selectedMUIClass ="MuiButtonBase-root MuiPickersDay-root Mui-selected MuiPickersDay-dayWithMargin css-bkrceb-MuiButtonBase-root-MuiPickersDay-root";
-                }
-                const renderOnce = true;
-                // DATA BEING USED:
-
-                // const changes =[{dog_id: 1, date_to_change: '2022-11-18', is_scheduled: false}, {dog_id: 7, date_to_change: '2022-11-22', is_scheduled: true}]
-
-                // const clientSchedule = {1: true, 2: true, 3: true, 4: true, 5: false}
-
-                // const dogs = [{dog_name: 'Cord', image: 'https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/domestic-dog_thumb_4x3.jpg', dog_id: 1, dog_notes: null, flag: null, regular: true}, {dog_name: 'Pamela', image: 'https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/domestic-dog_thumb_4x3.jpg', dog_id: 7, dog_notes: null, flag: null, regular: false}, {dog_name: 'Tami', image: 'https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/domestic-dog_thumb_4x3.jp', dog_id: 16, dog_notes: null, flag: null, regular: true}]
+                  }
 
                 return (
                     <Box
@@ -89,18 +71,15 @@ function ClientSchedule() {
                         className={ selectedMUIClass }
                         {...DayComponentProps} />
                       </Box>
-                      {/* Is this date in the current month ?*/}
+                    {/* Is this date in the current month ?*/}
                     {!DayComponentProps.outsideCurrentMonth ?
                       <div>
                         {/*  map through changes array: Is there a change for today? YES:, NO: check to see if it is a regularly scheduled day and render regularly scheduled dogs */}
-                        {/* changes =[{dog_id: 1, date_to_change: '2022-11-18', is_scheduled: true}, {dog_id: 7, date_to_change: '2022-11-22', is_scheduled: true}] */}
 
                         {changes && changes.map((change, index) => {
-                          // console.log('in changes. today is:',day.$D, 'the change is:',change)
                           // does the change date match today's date?
-                          // START OF FIRST CONDITIONAL:
                           return(
-                          <div> 
+                          <div key={change.id}> 
                             {/* is there a change scheduled today (month, day & year? */}
                             { JSON.stringify(DayComponentProps.day.$d) === JSON.stringify(dayjs(change.date_to_change).$d) ? 
                               <div>
@@ -112,10 +91,10 @@ function ClientSchedule() {
                                     <Box sx={{display: 'flex', flexDirection: 'row', flexGrow: '8', flexWrap: 'wrap',width: '4.5vw', alignContent: 'flex-start', justifyContent:'center', mb: 0, pt: 1.5}}>
                                     {dogs.map((dog, index)=> {
                                       return (
-                                      <>
+                                      <div key={dog.dog_id}>
                                       {/* render all the regularly scheduled dogs AND the dog added */}
                                         {dog.regular || change.dog_id === dog.dog_id ? 
-                                          <Avatar
+                                          <Avatar 
                                               key={dog.dog_id}
                                               sx={{width: '1.25vw', height: '1.25vw', mx: .25, fontSize: 13, border: 2, bgcolor: avatarColors[index], borderColor: avatarColors[index]}}
                                               alt={dog.dog_name[0]}
@@ -123,7 +102,7 @@ function ClientSchedule() {
                                           >
                                           </Avatar>
                                         : null}
-                                      </>)
+                                      </div>)
                                       })}
                                     </Box>
                                   :
@@ -131,7 +110,7 @@ function ClientSchedule() {
                                   <Box sx={{display: 'flex', flexDirection: 'row', flexGrow: '8', flexWrap: 'wrap',width: '4.5vw', alignContent: 'flex-start', justifyContent:'center', mb: 0, pt: 1.5}}>
                                     {dogs.map((dog, index)=> {
                                       return (
-                                      <>
+                                      <div key={dog.dog_id}>
                                       {/* render the dog(s) added */}
                                         {change.dog_id === dog.dog_id ? 
                                           <Avatar
@@ -142,7 +121,7 @@ function ClientSchedule() {
                                           >
                                           </Avatar>
                                         : null}
-                                      </>)
+                                      </div>)
                                       })}
                                     </Box>
                                   }</>
@@ -155,7 +134,7 @@ function ClientSchedule() {
                                     <Box sx={{display: 'flex', flexDirection: 'row', flexGrow: '8', flexWrap: 'wrap',width: '4.5vw', alignContent: 'flex-start', justifyContent:'center', mb: 0, pt: 1.5}}>
                                     {dogs.map((dog, index)=> {
                                       return (
-                                      <>
+                                      <div key={dog.dog_id}>
                                       {/* render all the regularly scheduled dogs EXCEPT the dog deleted */}
                                         {dog.regular && change.dog_id !== dog.dog_id ? 
                                           <Avatar
@@ -166,7 +145,7 @@ function ClientSchedule() {
                                           >
                                           </Avatar>
                                         : null}
-                                      </>)
+                                      </div>)
                                       })}
                                     </Box>
                                   :
@@ -174,7 +153,7 @@ function ClientSchedule() {
                                   <Box sx={{display: 'flex', flexDirection: 'row', flexGrow: '8', flexWrap: 'wrap',width: '4.5vw', alignContent: 'flex-start', justifyContent:'center', mb: 0, pt: 1.5}}>
                                     {dogs.map((dog, index)=> {
                                       return (
-                                      <>
+                                      <div key={dog.dog_id}>
                                       {/* render the dog(s) added */}
                                         {change.dog_id === dog.dog_id ? 
                                           <Avatar
@@ -185,7 +164,7 @@ function ClientSchedule() {
                                           >
                                           </Avatar>
                                         : null}
-                                      </>)
+                                      </div>)
                                       })}
                                     </Box>
                                   }</>
@@ -201,7 +180,7 @@ function ClientSchedule() {
                                   {dogs.map((dog, index)=> {
                                     
                                     return (
-                                    <>
+                                    <div key={dog.dog_id}>
                                     {/* render the dog(s) added */}
                                       { dog.regular && JSON.stringify(DayComponentProps.day.$d) !== JSON.stringify(dayjs('2022-11-22').$d) ? 
                                         <Avatar
@@ -212,7 +191,7 @@ function ClientSchedule() {
                                         >
                                         </Avatar>
                                       : null}
-                                    </>)
+                                    </div>)
                                     })}
                                 </Box>
                               :

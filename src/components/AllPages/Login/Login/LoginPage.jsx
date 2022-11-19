12c9cb0a5dd7ebbeffb3d907f5e5dfe7ee3ac211
Blake Smith <blake.smith.mn@gmail.com>
2022-11-19 09:55:17 -0600

    login button changed to direct to mobile dashboard if logged in on small screens

--- src/components/AllPages/Login/Login/LoginPage.jsx
+++ src/components/AllPages/Login/Login/LoginPage.jsx
@@ -1,19 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import {useSelector} from 'react-redux';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import { Box, Grid, Typography, Card, TextField, CardContent, Button } from '@mui/material';
 import './LoginPage.css';
 
 function LoginPage() {
+  const history = useHistory();
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const errors = useSelector(store => store.errors);
   const dispatch = useDispatch();
 
-  useEffect(()=> {
+  useEffect(() => {
     console.log('useEffect')
-    dispatch({ type: 'CLEAR_LOGIN_ERROR'});
-  },[]);
+    dispatch({ type: 'CLEAR_LOGIN_ERROR' });
+  }, []);
 
   const login = (event) => {
     event.preventDefault();
@@ -31,19 +33,38 @@ function LoginPage() {
     }
   }; // end login
 
+  const mobilelogin = (event) => {
+    // event.preventDefault();
+
+    if (username && password) {
+      dispatch({
+        type: 'LOGIN',
+        payload: {
+          username: username,
+          password: password,
+        },
+      });
+      history.push('/m/user');
+
+    } else {
+      dispatch({ type: 'LOGIN_INPUT_ERROR' });
+    }
+  }; // end login
+
+
   return (
     <Box className="login_container">
       <Grid container sx={{ justifyContent: "center", alignItems: "center", display: "flex", height: "80vh" }}>
         <Card className="login_card" sx={{ width: "30%", height: "70%" }}>
-          <Typography sx={{mt: 2}}align="center">
+          <Typography sx={{ mt: 2 }} align="center">
             Login
           </Typography>
-            {errors.loginMessage && (
-              <Typography sx={{fontSize: 10}} align="center" className="alert" role="alert">
-                {errors.loginMessage}
-              </Typography>
-            )}
-          <CardContent onSubmit={login} component ="form" className="formPanel" sx={{ justifyContent: "center", display: "flex", flexDirection: "column", alignItems: "center", height: '80%' }}>
+          {errors.loginMessage && (
+            <Typography sx={{ fontSize: 10 }} align="center" className="alert" role="alert">
+              {errors.loginMessage}
+            </Typography>
+          )}
+          <CardContent onSubmit={login} component="form" className="formPanel" sx={{ justifyContent: "center", display: "flex", flexDirection: "column", alignItems: "center", height: '80%' }}>
             <TextField
               margin="dense"
               label="username"
@@ -59,7 +80,10 @@ function LoginPage() {
               value={password}
               onChange={(event) => setPassword(event.target.value)}
             />
-            <Button type="submit">
+            <Button sx={{ display: { xs: 'block', sm: 'none', color: 'primary' } }} onClick={(event) => mobilelogin()}>
+              Login
+            </Button>
+            <Button type="submit" sx={{ display: { xs: 'none', sm: 'block', color: 'primary' } }}>
               Login
             </Button>
           </CardContent>

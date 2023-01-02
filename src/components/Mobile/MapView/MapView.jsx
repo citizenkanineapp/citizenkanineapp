536370d5264236dd2fb6925d82431d06beafad9c
Sam Freeman <samcfreeman888@gmail.com>
2023-01-01 17:03:48 -0800

    Switching to map branch

--- src/components/Mobile/MapView/MapView.jsx
+++ src/components/Mobile/MapView/MapView.jsx
@@ -4,6 +4,8 @@ import MobileTopNav from '../MobileNav/MobileTopNav';
 //MUI
 import { Typography, Grid } from '@mui/material';
 
+import { useState } from "react";
+
 
 
 
@@ -11,6 +13,8 @@ function MapView() {
   // during our time we were not able to get this working - so it has been disabled / removed from the nav bar
 
   const maptilerProvider = maptiler('JAXJd8TmToDTZ2jWk33G', 'bright')
+  const [hue, setHue] = useState(0)
+  const color = `hsl(${hue % 360}deg 39% 70%)`
 
   return (
     <Grid container
@@ -26,7 +30,12 @@ function MapView() {
       <Typography variant="h4" sx={{ m: 0 }}> Routes </Typography>
       <Grid item sx={{ width: '100%', height: '70rem' }}>
         <Map provider={maptilerProvider} defaultCenter={[44.92306458149502, -93.30491066897952]} defaultZoom={14}>
-          <Marker width={50} anchor={[50.879, 4.6997]} />
+          <Marker 
+            width={50} 
+            anchor={[44.92306458149502, -93.30491066897952]} 
+            // color={color} 
+            // onClick={setHue(hue+20)} 
+            />
         </Map>
       </Grid>
     </Grid>

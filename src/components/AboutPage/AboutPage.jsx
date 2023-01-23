e54557af0c95d22374adc352618e0c8ec34e0222
Sam Freeman <samcfreeman888@gmail.com>
2023-01-23 14:38:02 -0600

    QB sync now all flows through one button.  It checks for new clients, updates existing ones

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -15,15 +15,8 @@ function AboutPage() {
    location.href = "http://localhost:5000/api/oauth2/connect_handler";
   }
 
-  const originalSync = ()=>{
-    dispatch({ type: 'GET_QB_CUSTOMERS'})
-  }
-
-  const updateAllCustomers= ()=>{
-    dispatch({ type: 'UPDATE_ALL_QB_CUSTOMERS'})
-  }
-  const putRouteCustomers = ()=>{
-    dispatch({ type: 'PUT_ROUTE_QB_CUSTOMERS'})
+  const quickbooksSync = ()=>{
+    dispatch({ type: 'QUICKBOOKS_SYNC'})
   }
 
 
@@ -32,9 +25,9 @@ function AboutPage() {
     <div className="container">
       <h1>AboutPage</h1>
       <button onClick={connectQB}>Connect to QB</button>
-      <button onClick={originalSync}>Original Sync Test</button>
-      <button onClick={updateAllCustomers}>Pull New Customers</button>
-      <button onClick={putRouteCustomers}>Update All Customers</button>
+      {/* <button onClick={originalSync}>Original Sync Test</button> */}
+      {/* <button onClick={updateAllCustomers}>Pull New Customers</button> */}
+      <button onClick={quickbooksSync}>Sync With Quickbooks</button>
     </div>
   );
 }

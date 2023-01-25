69f7615b806c28dc07d88c70f987539955323ed2
Sam Freeman <samcfreeman888@gmail.com>
2023-01-24 19:43:28 -0600

    added google maps functionality to maps.  Still can't figure out tooltip solution.  Still need to decide on how to set map markers (what is flow of events?

--- src/components/Mobile/MapView/MapView.jsx
+++ src/components/Mobile/MapView/MapView.jsx
@@ -1,14 +1,15 @@
-import { Map, Marker } from "pigeon-maps"
+import { Map, Marker, Overlay } from "pigeon-maps"
 import { maptiler } from 'pigeon-maps/providers'
 import MobileTopNav from '../MobileNav/MobileTopNav';
 import { useDispatch, useSelector } from 'react-redux';
+import { usePopper } from 'react-popper';
 
 
 //MUI
 import { Typography, Grid } from '@mui/material';
 import Tooltip from '@mui/material/Tooltip';
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 function MapView() {
   
@@ -21,19 +22,31 @@ useEffect(() => {
   const [markers, setMarkers] = useState([])
  
   const populateMarkers = () => {
-   const markers = route.map(client => {
-    return {lat: Number(client.lat), long: Number(client.long), name: client.name}
-   })
+  const markers = route.map(client => {
+    return {lat: Number(client.lat), 
+            long: Number(client.long), 
+            name: client.client_name, 
+            street: client.street, 
+            zip: client.zip}
+       })
   //  console.log(markers)
    setMarkers(markers)
-   testFunction()
   }
-  const testFunction = () => {
-    console.log('markers right now ------>', markers)
-    // for (let marker of markers){
-    //   console.log(marker[lat])
-    // }
+
+    const openMap = async (dog) => {
+    // takes in address details and encodes them into URI 
+    const destination = encodeURIComponent(`${dog.street} ${dog.zip}`);
+    // based off of street address and city it pulls up a google map page
+    const link = `http://maps.google.com/?daddr=${destination}`;
+    window.open(link);
   }
+
+
+
+  const markerRef = useRef()
+  const tooltipRef = useRef()
+  const {styles, attributes} = usePopper(markerRef.current, tooltipRef.current)
+
 
   return (
     <Grid container
@@ -47,26 +60,20 @@ useEffect(() => {
 
       {/* <MobileTopNav /> */}
       <Typography variant="h4" sx={{ m: 0 }}> Routes </Typography>
-      <button onClick={populateMarkers}>Test</button>
+      {/* <button onClick={populateMarkers}>Test</button> */}
         <Grid item sx={{ width: '100%', height: '45rem' }}>
           <Map provider={maptilerProvider} defaultCenter={[44.92306458149522, -93.30491066897952]} defaultZoom={14}>
            {markers.map((oneMarker, index) => (
-            <Marker 
-              width={50} 
-              anchor={[oneMarker.lat, oneMarker.long]}
-              key={index}
-              onHover={() => console.log('hi')}
-              onClick={() => console.log('this is dog', oneMarker.name)}
-              />
+          
+             <Marker 
+                width={50} 
+                anchor={[oneMarker.lat, oneMarker.long]}
+                key={index}
+                
+                onClick={() => openMap(oneMarker)}
+                 /> 
+         
            ))}
-            {/* <Marker 
-              width={50} 
-              anchor={[44.92306458149502, -93.30491066897952]} 
-              />
-            <Marker 
-            width={50} 
-            anchor={[44.924, -93.305]} 
-            /> */}
           </Map>
         </Grid>
     </Grid>

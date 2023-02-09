a64fbd6128a9f56781142174fc3d9dc180c0121e
Dan Raskin <Danieltraskin@gmail.com>
2023-02-09 17:11:06 -0600

    one more database.sql edit

--- database.sql
+++ database.sql
@@ -67,7 +67,7 @@ CREATE TABLE "user" (
 INSERT INTO "user"
 	("username","password","admin", "emp_id","email")
 VALUES
-	('admin','$2a$10$UqOGOFQpFGSPEi/X1emtGOkqYQ.LD6SjSC03FZ2lZpb5EiBEbrfEu',true, null,'thecitizenkanine@gmail.com'),
+	('admin','$2a$10$UqOGOFQpFGSPEi/X1emtGOkqYQ.LD6SjSC03FZ2lZpb5EiBEbrfEu',true, null,'thecitizenkanine@gmail.com');
 	-- REMOVE BEFORE DEPLOYMENT
 	-- ('packleader','$2a$10$UqOGOFQpFGSPEi/X1emtGOkqYQ.LD6SjSC03FZ2lZpb5EiBEbrfEu',true, 2,null);
 	

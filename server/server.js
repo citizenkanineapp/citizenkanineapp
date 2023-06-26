1861545fd02a5184023cb0c08b7054cf9693d6ba
Dan Raskin <Danieltraskin@gmail.com>
2023-06-26 12:22:14 -0500

    log connecting schema

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 require('dotenv').config();
+const config = require('./modules/config');
 
 const app = express();
 
@@ -67,5 +68,5 @@ const PORT = process.env.PORT || 5000;
 
 /** Listen * */
 app.listen(PORT, () => {
-  console.log(`Listening on port: ${PORT}`);
+  console.log(`Listening on port: ${PORT}, connecting to db schema ${config.SCHEMA}`);
 });

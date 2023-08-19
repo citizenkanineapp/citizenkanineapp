b1d3a8fdb962196a74bafdc5926bf91c302c636e
Dan Raskin <Danieltraskin@gmail.com>
2023-08-18 22:28:41 -0500

    if QB sync fails due to msising address fields, pop-up alerts contain the problem client names

--- server/routes/quickbooks.router.js
+++ server/routes/quickbooks.router.js
@@ -188,6 +188,7 @@ router.post('/qbcustomers', rejectUnauthenticated, async (req, res) => {
   const client = await pool.connect();
   const customers = req.body // obj desctructing of QB data
     //geocoding for customers
+    // console.log('customers: ', customers)
   
   const api_key = process.env.map_api_key;
   const config = { headers: { Authorization: api_key } };
@@ -216,18 +217,25 @@ router.post('/qbcustomers', rejectUnauthenticated, async (req, res) => {
   }
   
   // returns TRUE if a address or town field is empty
+  let missingList = [];
   const checkCustomerAddressFields = (customers) => {
-      return customers.some(customer => !customer.street || !customer.city );
+    for (let client of customers) {
+      if (!client.street || !client.city) {
+        const name = `${client.first_name} ${client.last_name}`;
+        missingList.push(name);
+      }
+    }
+    return customers.some(customer => !customer.street || !customer.city);
   }
 
   let customersWithGeoStats
-  
   if (!checkCustomerAddressFields(customers)) {
     console.log('no fields empty')
     customersWithGeoStats = await GetGeoStats(customers)
   } else {
-    console.log('some fields empty')
-    return res.status(500).send({message:'Error! Missing customer address fields'});
+    // console.log('some fields empty', missingList)
+    const message = {message: `Error! The following customers are missing address fields: ${missingList.join(' ')}`}
+    return res.status(500).send(message);
   }
 
   let customersResult = processSchedule(customersWithGeoStats)

3e6f0bf00a3492a96413ddfb01974f118e703bc3
Dan Raskin <Danieltraskin@gmail.com>
2023-09-08 10:24:46 -0500

    fixed SQL queries in  mobile.router daily dogs generator to account for presence/absence of dog changes.

--- database.dev.sql
+++ database.dev.sql
@@ -214,6 +214,7 @@ CREATE TABLE ck_dev.daily_dogs (
 	"checked_in" BOOLEAN DEFAULT NULL,
 	"no_show" BOOLEAN DEFAULT NULL,
 	"cancelled" BOOLEAN DEFAULT NULL,
+	"index" SERIAL,
 	UNIQUE ("dog_id", "date")
 	);
 
